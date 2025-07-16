9fd7fe8df9b0828684690a7ff4d60f2a47e66507
jaewoo01 <87527657+jaewoo01@users.noreply.github.com>
2025-07-16 09:03:37 +0900

    Update poke_crwal_new.py

--- poke_crwal_new.py
+++ poke_crwal_new.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import selenium.webdriver as wb
 import time
 import csv
 import os
